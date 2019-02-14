c11afc7f812610c8aa79bc54230f49afdc69411b
nanxingzhang <939988325@qq.com>
2019-02-14 23:49:53 +0800

    LeetCode简单题第20题

---
+++ test20.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        dic={')':'(',']':'[','}':'{'}
+        stack=[]
+        for c in s:
+            if c in dic:
+                a=stack.pop() if stack else ' '
+                if a != dic[c]:
+                    return False
+            else:
+                stack.append(c)
+        return not stack
